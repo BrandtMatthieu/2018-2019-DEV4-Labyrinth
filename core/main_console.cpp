081da2ba007d811e19b7b64cb5758918ea45dfb5
44422 <44422@etu.he2.be>
2019-03-15 22:08:56 +0100

    Changes to the main class

--- core/main_console.cpp
+++ core/main_console.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <exception>
 
-#include "core/controller/include/controller_console.h"
+#include "controller/include/controller_console.h"
 #include "../console/include/consoleView.h"
 
-int main () {
-	try {
-		Labyrinth_44422::controller::Controller controller(Labyrinth_44422::console::ConsoleView());
-	} catch(std::exception exception) {
-		std::cout 	<< "Labyrinth a rencontré une erreur et doit fermer" << std::endl
-					<< exception.what() << std::endl;
+/**
+ * Entry point for the console version of Labyrinth game
+ * @return 0 exit code if program encounters no errors
+*/
+int main() {
+
+    try {
+		Labyrinth_44422::controller::ControllerConsole controller(new Labyrinth_44422::console::ConsoleView(true));
+		controller.start();
+	} catch(std::exception & exception) {
+		std::cout << "Labyrinth a rencontre une erreur et doit fermer." << std::endl << exception.what() << std::endl;
 		return -1;
 	}
+
 	return 0;
 }
