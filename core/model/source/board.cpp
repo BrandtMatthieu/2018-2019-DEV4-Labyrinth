b344d1bdd3f8024fbdce184af91c52a686d47056
smrman <vinoa.89.jeux@gmail.com>
2019-03-21 18:21:14 +0100

    Added the pathfinding method in the Board class

--- core/model/source/board.cpp
+++ core/model/source/board.cpp
@@ -1,10 +1,43 @@
 #include <stdexcept>
+#include <math.h>
 
 #include "../include/board.h"
 #include "../include/tile.h"
 
 namespace Labyrinth_44422 {
 	namespace model {
+		
+		/**
+		 * Finds a tile in a vector of tiles and returns it's index
+		 * @param myVector the vector where to search
+		 * @param tile the tile to search for
+		 * @return the index of the tiles in the provided vector
+		 */
+		unsigned int Board::indexOf(std::vector<Tile *> & myVector, Tile * tile) const {
+			if(this->includes(myVector, tile)) {
+				for(unsigned int i = 0; i < myVector.size(); i++) {
+					if(this->tiles[i] == tile) {
+						return i;
+					}
+				}
+			}
+			return 0;
+		}
+		
+		/**
+		 * Returns true if an element exists in a vector
+		 * @param myVector the vector where to search
+		 * @param tile the tile to search for
+		 * @return true if an element exists in a vector
+		 */
+		bool Board::includes(std::vector<Tile *> & myVector, Tile * tile) const {
+			for(int i = 0; i < myVector.size(); i++) {
+				if(this->tiles[i] == tile) {
+					return true;
+				}
+			}
+			return false;
+		}
 
 		/**
 		 * Creates a new board for the Labyrinth game
@@ -96,8 +129,13 @@ namespace Labyrinth_44422 {
 		 * @param position the position of where to set the tile
 		 * @param tile the tile to set at the provided position
 		 */
-		void Board::setTile(const Position & position, Tile const * tile) {
-			// TODO
+		void Board::setTile(const Position & position, const Tile * const tile) {
+			if(position.getX() + position.getY() * this->getSizeHorizontal() > this->getSizeHorizontal() * this->getSizeVertical()
+			|| position.getX() + position.getY() * this->getSizeHorizontal() > this->tiles.size()) {
+				throw std::invalid_argument("Position is out of bounds");
+			} else {
+				this->tiles[position.getX() + position.getY() * this->getSizeHorizontal()] = const_cast<Tile *>(tile);
+			}
 		}
 		
 		/**
@@ -107,8 +145,22 @@ namespace Labyrinth_44422 {
 		 * @return true if a tile can be inserted at the provided position
 		 */
 		bool Board::canInsertTile(const Position & position, const InsertSide & side) const {
-			// TODO
-			return false;
+			bool lineMovable = true;
+			switch(side) {
+				case InsertSide::UP:
+				case InsertSide::DOWN:
+					for(unsigned int i = position.getX(); i < this->tiles.size(); i = i + this->getSizeHorizontal()) {
+						lineMovable = lineMovable && this->tiles.at(i)->isMovable();
+					}
+					break;
+				case InsertSide::RIGHT:
+				case InsertSide::LEFT:
+					for(unsigned int i = position.getY() * this->getSizeHorizontal(); i < (position.getY() + 1) * this->getSizeHorizontal(); i++) {
+						lineMovable = lineMovable && this->tiles.at(i)->isMovable();
+					}
+					break;
+			}
+			return lineMovable;
 		}
 		
 		/**
@@ -117,7 +169,7 @@ namespace Labyrinth_44422 {
 		 * @param tile the tile to insert
 		 * @param side what side to insert the tile at
 		 */
-		void Board::insertTile(const Position & position, Tile const * tile, const InsertSide & side) {
+		void Board::insertTile(const Position & position, const Tile * const tile, const InsertSide & side) {
 			// TODO
 		}
 		
@@ -127,9 +179,101 @@ namespace Labyrinth_44422 {
 		 * @param player the player
 		 * @return true if a player can go to a provided position
 		 */
-		bool Board::canPlayerGoTo(const Position & position, Player const * player) const {
-			// TODO
-			return false;
+		bool Board::canGoToFrom(Position & from, Position & to) {
+			std::vector<Tile *> tilesToCheck;
+			std::vector<Tile *> tilesPossible;
+			std::vector<Tile *> tilesNotPossible;
+			
+			tilesPossible.push_back(this->tiles[from.getX() + from.getY() * (this->maxPosition.getX())]);
+			tilesToCheck.push_back(this->tiles[from.getX() + from.getY() * (this->maxPosition.getX())]);
+			
+			unsigned int currentTileX;
+			unsigned int currentTileY;
+			
+			while (!tilesToCheck.empty()) {
+				Tile * currentTile = tilesToCheck[0];
+				
+				currentTileX = this->indexOf(this->tiles, currentTile) % this->maxPosition.getX();
+				currentTileY = this->indexOf(this->tiles, currentTile) / this->maxPosition.getX();
+				
+				unsigned int index;
+				if (currentTileX + 1 < this->maxPosition.getX()) {
+					index = (currentTileX + 1) + currentTileY * this->maxPosition.getX();
+					if (currentTile->getPathRIGHT() && this->tiles[index]->getPathLEFT() && !this->includes(tilesPossible, this->tiles[index])) {
+						if (!this->includes(tilesToCheck, this->tiles[index])) {
+							tilesToCheck.push_back(this->tiles[index]);
+						}
+						if (!this->includes(tilesPossible, this->tiles[index])) {
+							tilesPossible.push_back(this->tiles[index]);
+						}
+						if (this->includes(tilesNotPossible, this->tiles[index])) {
+							tilesNotPossible.erase(tilesNotPossible.begin() + this->indexOf(tilesNotPossible, this->tiles[index]));
+						}
+					} else {
+						if (!this->includes(tilesNotPossible, this->tiles[index])) {
+							tilesNotPossible.push_back(this->tiles[index]);
+						}
+					}
+				}
+				if (currentTileX - 1 >= 0) {
+					index = (currentTileX - 1) + currentTileY * this->maxPosition.getX();
+					if (currentTile->getPathLEFT() && this->tiles[index]->getPathRIGHT() && !this->includes(tilesPossible, this->tiles[index])) {
+						if (!this->includes(tilesToCheck, this->tiles[index])) {
+							tilesToCheck.push_back(this->tiles[index]);
+						}
+						if (!this->includes(tilesPossible, this->tiles[index])) {
+							tilesPossible.push_back(this->tiles[index]);
+						}
+						if (this->includes(tilesNotPossible, this->tiles[index])) {
+							tilesNotPossible.erase(tilesNotPossible.begin() + this->indexOf(tilesNotPossible, this->tiles[index]));
+						}
+					} else {
+						if (!this->includes(tilesNotPossible, this->tiles[index])) {
+							tilesNotPossible.push_back(this->tiles[index]);
+						}
+					}
+				}
+				if (currentTileY - 1 >= 0) {
+					index = currentTileX + (currentTileY - 1) * this->maxPosition.getX();
+					if (currentTile->getPathUP() && this->tiles[index]->getPathDOWN() && !this->includes(tilesPossible, this->tiles[index])) {
+						if (!this->includes(tilesToCheck, this->tiles[index])) {
+							tilesToCheck.push_back(this->tiles[index]);
+						}
+						if (!this->includes(tilesPossible, this->tiles[index])) {
+							tilesPossible.push_back(this->tiles[index]);
+						}
+						if (this->includes(tilesNotPossible, this->tiles[index])) {
+							tilesNotPossible.erase(tilesNotPossible.begin() + this->indexOf(tilesNotPossible, this->tiles[index]));
+						}
+					} else {
+						if (!this->includes(tilesNotPossible, this->tiles[index])) {
+							tilesNotPossible.push_back(this->tiles[index]);
+						}
+					}
+				}
+				if (currentTileY + 1 < this->maxPosition.getY()) {
+					index = currentTileX + (currentTileY + 1) * this->maxPosition.getX();
+					if (currentTile->getPathDOWN() && this->tiles[index]->getPathUP() && !this->includes(tilesPossible, this->tiles[index])) {
+						if (!this->includes(tilesToCheck, this->tiles[index])) {
+							tilesToCheck.push_back(this->tiles[index]);
+						}
+						if (!this->includes(tilesPossible, this->tiles[index])) {
+							tilesPossible.push_back(this->tiles[index]);
+						}
+						if (this->includes(tilesNotPossible, this->tiles[index])) {
+							tilesNotPossible.erase(tilesNotPossible.begin() + this->indexOf(tilesNotPossible, this->tiles[index]));
+						}
+					} else {
+						if (!this->includes(tilesNotPossible, this->tiles[index])) {
+							tilesNotPossible.push_back(this->tiles[index]);
+						}
+					}
+				}
+				tilesToCheck.erase(tilesToCheck.begin() + 0);
+			}
+			
+			
+			return this->includes(tilesPossible, this->tiles[to.getX() + to.getY() * this->maxPosition.getX()]);
 		}
 	}
 }
